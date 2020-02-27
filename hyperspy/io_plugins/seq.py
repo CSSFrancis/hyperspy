6cbe03efd9718f054c9b4963dfa402b81e699ad5
Carter Francis <csfrancis@wisc.edu>
2020-02-26 22:15:40 -0600

    Seq loading works.  Might still make some slight modifications if necessary

--- hyperspy/io_plugins/seq.py
+++ hyperspy/io_plugins/seq.py
@@ -1,6 +1,6 @@
 # Plugin characteristics
 # ----------------------
-format_name = 'seq seqential file'
+format_name = 'seq sequential file'
 description = """The file format used by StreamPix
 and an output for Direct Electron detectors"""
 full_support = False
@@ -16,15 +16,7 @@ writes = False
 
 import os
 import logging
-import dateutil.parser
-
 import numpy as np
-import traits.api as t
-
-import hyperspy.misc.io.utils_readfile as iou
-from hyperspy.exceptions import DM3TagIDError, DM3DataTypeError, DM3TagTypeError
-import hyperspy.misc.io.tools
-from hyperspy.misc.utils import DictionaryTreeBrowser
 from hyperspy.docstrings.signal import OPTIMIZE_ARG
 import struct
 
@@ -32,132 +24,154 @@ import struct
 _logger = logging.getLogger(__name__)
 
 data_types = {8: np.uint8, 16: np.uint16, 32: np.uint32}  # Stream Pix data types
+
+
 class SeqReader(object):
     """ Class to read .seq files. File format from StreamPix and Output for Direct Electron Cameras
     """
-
-    def __init__(self, f):
-        self.f = f
-        self.metadata_dict = None
-        self.frame_width = None
-        self.frame_height = None
-        self.frame_length =None
-        self.num_frames = None
-        self.img_bytes = None
+    def __init__(self, file):
+        self.file = file
+        self.metadata_dict = {}
+        self.axes= []
+        self.image_dict = {}
         self.dark_ref = None
         self.gain_ref = None
-        self.fps = None
-        self.bit_depth = None
         self.image_dtype_list = None
 
     def _get_dark_ref(self):
+        # The dark and gain references are saved as 32 bit floats. I can either change the image to 32 bit float
+        # or change the dark and gain references to 16 bit int images.
         try:
-            with open (self.f+".dark.mrc") as dark_ref:
-                bytes = dark_ref.read(self.frame_width * self.frame_height * 4)
-                self.dark_ref = np.frombuffer(bytes, dtype=np.float32)
+            with open (self.file + ".dark.mrc", mode='rb') as file:
+                file.seek(0)
+                read_bytes = file.read(8)
+                frame_width = struct.unpack('<i', read_bytes[0:4])[0]
+                frame_height = struct.unpack('<i', read_bytes[4:8])[0]
+                file.seek(256 * 4)
+                bytes = file.read(frame_width * frame_height * 4)
+                self.dark_ref = np.array(
+                    np.round(
+                        np.reshape(
+                            np.frombuffer(bytes, dtype=np.float32),(self.image_dict["ImageWidth"],
+                                                                    self.image_dict["ImageHeight"]))),
+                    dtype=self.image_dict["ImageBitDepth"])
         except FileNotFoundError:
             print("No Dark Reference image found.  The Dark reference should be in the same directory "
-                  "as the image and have the form testfile.seq.dark.mrc")
+                  "as the image and have the form xxx.seq.dark.mrc")
 
     def _get_gain_ref(self):
         try:
-            with open (self.f+".gain.mrc") as dark_ref:
-                bytes = dark_ref.read(self.frame_width * self.frame_height * 4)
-                self.dark_ref = np.frombuffer(bytes, dtype=np.float32)  # 32 bit float image gain and dark
-                # Images kept in linear form.
+            with open (self.file+".gain.mrc", mode='rb') as file:
+                file.seek(0)
+                read_bytes = file.read(8)
+                frame_width = struct.unpack('<i', read_bytes[0:4])[0]
+                frame_height = struct.unpack('<i', read_bytes[4:8])[0]
+                file.seek(256 * 4)
+                bytes = file.read(frame_width * frame_height * 4)
+                self.gain_ref = np.array(
+                    np.round(
+                        np.reshape(
+                            np.frombuffer(bytes, dtype=np.float32), (self.image_dict["ImageWidth"],
+                                                                     self.image_dict["ImageHeight"]))),
+                    dtype=self.image_dict["ImageBitDepth"])  # Casting to 16 bit ints
         except FileNotFoundError:
-            print("No gain Reference image found.  The Dark reference should be in the same directory "
-                  "as the image and have the form testfile.seq.dark.mrc")
+            print("No gain reference image found.  The Gain reference should be in the same directory "
+                  "as the image and have the form xxx.seq.gain.mrc")
 
     def parse_header(self):
-        print(self.f)
-
-        with open(self.f, mode='rb') as file:  # b is important -> binary
+        with open(self.file, mode='rb') as file:  # b is important -> binary
             file.seek(548)
-            image_info_dtype= [(("ImageWidth"),("<u4")),
-                               (("ImageHeight"), ("<u4")),
-                               (("ImageBitDepth"), ("<u4")),
-                               (("ImageBitDepthReal"), ("<u4"))]
+            image_info_dtype = [(("ImageWidth"),("<u4")),
+                                (("ImageHeight"), ("<u4")),
+                                (("ImageBitDepth"), ("<u4")),
+                                (("ImageBitDepthReal"), ("<u4"))]
             image_info = np.fromfile(file, image_info_dtype, count=1)[0]
-            self.frame_width = image_info[0]
-            self.frame_height = image_info[1]
-            self.bit_depth = data_types[image_info[2]]  # image bit depth
-            self.bit_depth_real = image_info[3]  # actual recorded bit depth
-            self.frame_length = self.frame_height*self.frame_width
-            _logger.info('Each frame is %i x %i pixels', (self.frame_width, self.frame_width))
+            self.image_dict['ImageWidth'] = image_info[0]
+            self.image_dict['ImageHeight'] = image_info[1]
+            self.image_dict['ImageBitDepth'] = data_types[image_info[2]]  # image bit depth
+            self.image_dict["ImageBitDepthReal"] = image_info[3]  # actual recorded bit depth
+            self.image_dict["FrameLength"] = image_info[0] * image_info[1]
+            _logger.info('Each frame is %i x %i pixels', (image_info[0], image_info[1]))
             file.seek(572)
 
             read_bytes = file.read(4)
-            self.num_frames = struct.unpack('<i', read_bytes)[0]
-            _logger.info('%i number of frames found', self.num_frames)
+            self.image_dict["NumFrames"] = struct.unpack('<i', read_bytes)[0]
+            _logger.info('%i number of frames found', self.image_dict["NumFrames"])
 
             file.seek(580)
             read_bytes = file.read(4)
-            self.img_bytes = struct.unpack('<L', read_bytes[0:4])[0]
+            self.image_dict["ImgBytes"] = struct.unpack('<L', read_bytes[0:4])[0]
 
             file.seek(584)
             read_bytes = file.read(8)
-            self.fps = struct.unpack('<d', read_bytes)[0]
-            _logger.info('Image acquired at %i frames per second', self.fps)
-
+            self.image_dict["FPS"] = struct.unpack('<d', read_bytes)[0]
         return
 
-    def parse_metadata(self):
-        metadata = {'General': {'original_filename': os.path.split(self.f)[1],},
-                    "Signal": {'signal_type': "Signal2D"}, }
+    def parse_metadata_file(self):
+        """ This reads the metadata from the .metadata file """
         try:
-            with open(self.f+".metadata") as meta:
-                pass
-        except FileNotFoundError:
-            print("No gain Reference image found.  The Dark reference should be in the same directory "
-                  "as the image and have the form testfile.seq.dark.mrc")
-        return metadata
+            with open(self.file + ".metadata", 'rb') as meta:
+                meta.seek(320)
+                image_info_dtype = [(("SensorGain"), (np.float64)),
+                                    (("Magnification"), (np.float64)),
+                                    (("PixelSize"), (np.float64)),
+                                    (("CameraLength"), (np.float64)),
+                                    (("DiffPixelSize"), (np.float64))]
+                m = np.fromfile(meta, image_info_dtype, count=1)[0]
+                self.metadata_dict["SensorGain"] = m[0]
+                self.metadata_dict["Magnification"] = m[1]
+                self.metadata_dict["PixelSize"] = m[2]
+                self.metadata_dict["CameraLength"] = m[3]
+                self.metadata_dict["DiffPixelSize"] = m[4]
 
-    def parse_axes(self):
-        axes = []
-        axes.append({
-            'name':'kx',
-            'offset': 0,
-            'scale': 1,
-            'size': self.frame_width,
-            'navigate': False,
-            'index_in_array': 0})
-        axes.append({
-            'name': 'ky',
-            'offset': 0,
-            'scale': 1,
-            'size': self.frame_height,
-            'navigate': False,
-            'index_in_array': 1})
-        axes.append({
-            'name': 'x',
-            'offset': 0,
-            'scale': 1,
-            'size': self.num_frames,
-            'navigate': True,
-            'index_in_array': 2})
-        try:
-            with open(self.f+".metadata") as meta:
-                pass
         except FileNotFoundError:
-            print("No Gain Reference image found.  The Gain reference should be in the same directory "
-                  "as the image and have the form testfile.seq.gain.mrc")
+            print("No metadata file.  The metadata should be in the same directory "
+                  "as the image and have the form xxx.seq.metadata")
+        return
+
+    def create_axes(self):
+        axes = []
+        axes.append({'name':'time', 'offset': 0, 'scale': 1,'size': self.image_dict["NumFrames"],
+                     'navigate': True, 'index_in_array': 0})
+        axes.append({'name': 'ky', 'offset': 0, 'scale': 1, 'size': self.image_dict["ImageHeight"],
+                     'navigate': False, 'index_in_array': 1})
+        axes.append({'name': 'kx', 'offset': 0, 'scale': 1, 'size': self.image_dict["ImageWidth"],
+                     'navigate': False, 'index_in_array': 2})
+
+        if self.metadata_dict is not {} and self.metadata_dict["PixelSize"] != 0:
+            # need to still determine a way to properly set units and scale
+            axes[1]['scale'] = self.metadata_dict["PixelSize"]
+            axes[2]['scale'] = self.metadata_dict["PixelSize"]
+
+        axes[0]['scale'] = 1/self.image_dict["FPS"]
 
         return axes
 
-    def _get_image(self):
-        with open(self.f, mode='rb') as file:
-            dtype_list = [(("Array"), self.bit_depth, (self.frame_width, self.frame_height)),
-                          (("t_value"),("<u4")),
-                          (("Miliseconds"), ("<u2")),
-                          (("Microseconds"), ("<u2"))
-                          ]
-            data = np.empty(self.num_frames, dtype=dtype_list)  # creating an empty array
-            file.seek(8192)
-            for i in range(self.num_frames):
-                file.seek(8192+i*self.img_bytes)
+    def create_metadata(self):
+        metadata = {'General': {'original_filename': os.path.split(self.file)[1]},
+                    'Signal': {'signal_type': 'Signal2D'}}
+        if self.metadata_dict is not {}:
+            metadata['Acquisition_instrument'] = {'TEM':
+                                                      {'camera_length': self.metadata_dict["CameraLength"],
+                                                       'magnification': self.metadata_dict["Magnification"]}}
+        return metadata
+
+    def get_image_data(self):
+        with open(self.file, mode='rb') as file:
+            dtype_list = [(("Array"), self.image_dict["ImageBitDepth"],
+                           (self.image_dict["ImageWidth"], self.image_dict["ImageHeight"]))]
+            # (("t_value"),("<u4")), (("Milliseconds"), ("<u2")), (("Microseconds"), ("<u2"))]
+            data = np.empty(self.image_dict["NumFrames"], dtype=dtype_list)  # creating an empty array
+            max_pix = 2**self.image_dict["ImageBitDepthReal"]
+            for i in range(self.image_dict["NumFrames"]):
+                file.seek(8192 + i * self.image_dict["ImgBytes"])
                 if self.dark_ref is not None and self.gain_ref is not None:
-                    data[i] = (np.fromfile(file, dtype_list, count=1) - self.dark_ref) * self.gain_ref
+                    d = np.fromfile(file, dtype_list, count=1)
+                    d["Array"] = (d["Array"] - self.dark_ref)
+                    d["Array"][d["Array"] > max_pix] = 0
+                    d["Array"] = d["Array"] * self.gain_ref  # Numpy doesn't check for overflow.
+                    # There might be a better way to do this. OpenCV has a method for subtracting
+                    data[i] = d
                 else:
                     data[i] = np.fromfile(file, dtype_list, count=1)
         return data["Array"]
@@ -166,11 +180,13 @@ class SeqReader(object):
         if lazy:
             from dask import delayed
             from dask.array import from_delayed
-            val = delayed(self._get_image, pure=True)
-            dt =(("Array"), self.bit_depth, (self.frame_width, self.frame_height))
-            data = from_delayed(val, shape=self.num_frames, dtype=)
+            val = delayed(self.get_image_data(), pure=True)
+            data = from_delayed(val, shape=(self.image_dict["NumFrames"],self.image_dict["ImageWidth"],
+                                            self.image_dict["ImageHeight"]), dtype=self.image_dict["ImageBitDepth"])
+            #data = from_delayed(val, shape=(self.image_dict["NumFrames"],self.image_dict["ImageWidth"],
+             #                               self.image_dict["ImageHeight"]), dtype=self.image_dict["ImageBitDepth"])
         else:
-            data = self._get_image()
+            data = self.get_image_data()
         return data
 
 
@@ -188,8 +204,9 @@ def file_reader(filename, lazy=False):
     seq.parse_header()
     seq._get_dark_ref()
     seq._get_gain_ref()
-    metadata = seq.parse_metadata()
-    axes = seq.parse_axes()
+    seq.parse_metadata_file()
+    axes = seq.create_axes()
+    metadata = seq.create_metadata()
     data = seq.read_data(lazy=lazy)
     dictionary = {
         'data': data,
--- hyperspy/tests/io/test_streampix.py
+++ hyperspy/tests/io/test_streampix.py
@@ -2,6 +2,7 @@ import os
 
 from unittest import TestCase
 import matplotlib.pyplot as plt
+plt.show()
 import pytest
 import numpy as np
 
@@ -11,7 +12,7 @@ from hyperspy.misc.test_utils import assert_warns
 MY_PATH = os.path.dirname(__file__)
 
 
-class TestSEQReader2(TestCase):
+class TestSEQReader(TestCase):
 
     def setUp(self):
         self.seq = os.path.join(MY_PATH, 'seq_files')
@@ -20,11 +21,18 @@ class TestSEQReader2(TestCase):
 
     def test_load_seq_lazy(self):
         s = load(self.file0, lazy=True)
-        print(s.data)
+        print("the size is :", np.shape(s.inav[1].data.compute()))
+        print(s.axes_manager)
+        s.plot()
+        plt.ion()
+        plt.show(block=False)
+        plt.draw()
+        plt.show()
 
     def test_load_seq(self):
         s = load(self.file0, lazy=False)
-        s.plot()
         print(np.shape(s.data))
-        print(s.inav[1].data)
-        plt.show(block=True)
+        print(np.shape(s.inav[1].data))
+        print(s.axes_manager)
+        s.plot()
+        plt.show()
