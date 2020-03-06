a98dd22caebb25e370aa7325c75b4935da2e3507
shaw <csfrancis@wisc.edu>
2020-03-06 16:07:39 -0600

    Lazy Gain and dark reference correction don't work properly.

--- hyperspy/io_plugins/seq.py
+++ hyperspy/io_plugins/seq.py
@@ -19,6 +19,7 @@ import logging
 import numpy as np
 from hyperspy.docstrings.signal import OPTIMIZE_ARG
 import struct
+import dask
 
 
 _logger = logging.getLogger(__name__)
@@ -31,6 +32,7 @@ class SeqReader(object):
     """
     def __init__(self, file):
         self.file = file
+        self.open_file=None
         self.metadata_dict = {}
         self.axes= []
         self.image_dict = {}
@@ -109,14 +111,24 @@ class SeqReader(object):
                                     (("PixelSize"), (np.float64)),
                                     (("CameraLength"), (np.float64)),
                                     (("DiffPixelSize"), (np.float64)),
-                                    (("ScaleBarIntLength"), ("<i"))]
+                                    (("ScaleBarIntLength"), ("<i4")),
+                                    (("ScaleBarUnit"), "<i1",(10))]
+
                 m = np.fromfile(meta, image_info_dtype, count=1)[0]
-                print(m)
+                print("The Metadata is: ", m)
                 self.metadata_dict["SensorGain"] = m[0]
                 self.metadata_dict["Magnification"] = m[1]
                 self.metadata_dict["PixelSize"] = m[2]
                 self.metadata_dict["CameraLength"] = m[3]
                 self.metadata_dict["DiffPixelSize"] = m[4]
+                try:
+                    scale = ''.join([chr(a) for a in m[6] if a!=0]).split()
+                    print(scale)
+                    self.metadata_dict["PixelUnit"] = scale[1]
+                except(IndexError):
+                    _logger.info('No Scale found')
+                    self.metadata_dict["PixelUnit"]=None
+
                 meta.seek(364)
 
         except FileNotFoundError:
@@ -128,20 +140,24 @@ class SeqReader(object):
         axes = []
         axes.append({'name': 'time', 'offset': 0, 'scale': 1, 'size': self.image_dict["NumFrames"],
                      'navigate': True, 'index_in_array': 0})
-        axes.append({'name': 'ky', 'offset': 0, 'scale': 1, 'size': self.image_dict["ImageHeight"],
-                     'navigate': False, 'index_in_array': 1})
-        axes.append({'name': 'kx', 'offset': 0, 'scale': 1, 'size': self.image_dict["ImageWidth"],
-                     'navigate': False, 'index_in_array': 2})
+        axes.append({'name': 'y', 'offset': 0, 'scale': 1, 'size': self.image_dict["ImageHeight"],
+                     'navigate': False, 'index_in_array': 1, 'units':"undefined"})
+        axes.append({'name': 'x', 'offset': 0, 'scale': 1, 'size': self.image_dict["ImageWidth"],
+                     'navigate': False, 'index_in_array': 2, 'units': "undefined"})
 
         if self.metadata_dict is not {} and self.metadata_dict["PixelSize"] != 0:
             # need to still determine a way to properly set units and scale
             axes[1]['scale'] = self.metadata_dict["PixelSize"]
             axes[2]['scale'] = self.metadata_dict["PixelSize"]
+            axes[1]['units'] = self.metadata_dict["PixelUnit"]
+            axes[2]['units'] = self.metadata_dict["PixelUnit"]
 
         if self.metadata_dict is not {} and self.metadata_dict["DiffPixelSize"] != 0:
             # need to still determine a way to properly set units and scale
             axes[1]['scale'] = self.metadata_dict["DiffPixelSize"]
             axes[2]['scale'] = self.metadata_dict["DiffPixelSize"]
+            axes[1]['name'] = self.metadata_dict["kx"]
+            axes[2]['name'] = self.metadata_dict["ky"]
 
         axes[0]['scale'] = 1/self.image_dict["FPS"]
 
@@ -156,37 +172,50 @@ class SeqReader(object):
                                                        'magnification': self.metadata_dict["Magnification"]}}
         return metadata
 
-    def get_image_data(self):
+    def _apply_correction(self, start, dtype_list):
+        self.open_file.seek(start)
+        if self.dark_ref is not None and self.gain_ref is not None:
+            d = np.fromfile(self.open_file, dtype_list, count=1)
+            holder = d["Array"].astype(np.float32) - self.dark_ref
+            holder[holder < 0] = 0
+            d["Array"] = np.round((holder * self.gain_ref)).astype(np.int16)
+        else:
+            d = np.fromfile(self.open_file, dtype_list, count=1)
+        return d["Array"][0]
+
+    def get_image_data(self, lazy=False):
         # Currently changing the images from 16 bit integers to 32 bit floats because of the dark and gain reference
         # correction. And then changes the images back.
-        with open(self.file, mode='rb') as file:
-            dtype_list = [(("Array"), self.image_dict["ImageBitDepth"],
-                           (self.image_dict["ImageWidth"], self.image_dict["ImageHeight"]))]
-            # (("t_value"),("<u4")), (("Milliseconds"), ("<u2")), (("Microseconds"), ("<u2"))]
-            data = np.empty(self.image_dict["NumFrames"], dtype=dtype_list)  # creating an empty array
-            max_pix = 2**self.image_dict["ImageBitDepthReal"]
-            for i in range(self.image_dict["NumFrames"]):
-                file.seek(8192 + i * self.image_dict["ImgBytes"])
-                if self.dark_ref is not None and self.gain_ref is not None:
-                    d = np.fromfile(file, dtype_list, count=1)
-                    holder = d["Array"].astype(np.float32)-self.dark_ref
-                    holder[holder < 0] = 0
-                    d["Array"] = np.round((holder * self.gain_ref)).astype(np.int16)
-                    data[i] = d
-                else:
-                    data[i] = np.fromfile(file, dtype_list, count=1)
-        return data["Array"]
+        self.open_file = open(self.file, mode='rb')
+        dtype_list = [(("Array"), self.image_dict["ImageBitDepth"],
+                       (self.image_dict["ImageWidth"], self.image_dict["ImageHeight"]))]
+        # (("t_value"),("<u4")), (("Milliseconds"), ("<u2")), (("Microseconds"), ("<u2"))]
+        starts = [8192 + i * self.image_dict["ImgBytes"]for i in range(self.image_dict["NumFrames"])]
+        if lazy:
+            from dask import delayed
+            from dask.array import from_delayed, stack
+            values = [delayed(self._apply_correction)(start=start, dtype_list=dtype_list) for start in starts]
+            arrays = [from_delayed(v,
+                                   shape=(self.image_dict["ImageWidth"], self.image_dict["ImageHeight"]),
+                                   dtype=self.image_dict["ImageBitDepth"])
+                      for v in values]
+            data = stack(arrays)
+        else:
+            data = np.array([self._apply_correction(start=start, dtype_list=dtype_list) for start in
+                     starts])
 
-    def read_data(self, lazy=False):
+        return data
+
+    """def read_data(self, lazy=False):
         if lazy:
             from dask import delayed
             from dask.array import from_delayed
-            val = delayed(self.get_image_data(), pure=True)
+            val = delayed(self.get_image_data, pure=True)()
             data = from_delayed(val, shape=(self.image_dict["NumFrames"], self.image_dict["ImageWidth"],
                                             self.image_dict["ImageHeight"]), dtype=self.image_dict["ImageBitDepth"])
         else:
             data = self.get_image_data()
-        return data
+        return data"""
 
 
 def file_reader(filename, lazy=False):
@@ -206,7 +235,7 @@ def file_reader(filename, lazy=False):
     seq.parse_metadata_file()
     axes = seq.create_axes()
     metadata = seq.create_metadata()
-    data = seq.read_data(lazy=lazy)
+    data = seq.get_image_data(lazy=lazy)
     dictionary = {
         'data': data,
         'metadata': metadata,
--- hyperspy/tests/io/test_streampix.py
+++ hyperspy/tests/io/test_streampix.py
@@ -17,6 +17,7 @@ class TestSEQReader(TestCase):
     def setUp(self):
         self.seq = os.path.join(MY_PATH, 'seq_files')
         self.file0 = os.path.join(self.seq, 'test.seq')
+        #self.file0 = '/media/hdd/home/data/TEM/MgNiY-2020-01-17/13-50-27.443.seq'
         self.file1 = os.path.join(self.seq, 'test1.seq')
         print(self.file0)
         self.reader0 = SeqReader(file=self.file0)
@@ -43,19 +44,38 @@ class TestSEQReader(TestCase):
     def test_apply_gain_dark(self):
         # With dark and gain reference
         self.reader0.parse_header()
-        uncorrected_data=self.reader0.get_image_data()
+        uncorrected_data = self.reader0.get_image_data()
         self.reader0._get_gain_ref()
         self.reader0._get_dark_ref()
         corrected_data = self.reader0.get_image_data()
         uncorrected_data = np.array(uncorrected_data, dtype=np.float32)
-        print(uncorrected_data)
         true_correction = (uncorrected_data - self.reader0.dark_ref)*self.reader0.gain_ref
         true_correction[true_correction < 0] = 0
         true_correction = np.round(true_correction)
+
         ntest.assert_array_almost_equal(true_correction, corrected_data)
 
+    def test_apply_gain_dark_lazy(self):
+        # With dark and gain reference
+        self.reader0.parse_header()
+        uncorrected_data = self.reader0.get_image_data(lazy=True).compute()
+        self.reader0._get_gain_ref()
+        self.reader0._get_dark_ref()
+        corrected_data = self.reader0.get_image_data(lazy=True).compute()
+        uncorrected_data = np.array(uncorrected_data, dtype=np.float32)
+        #print(uncorrected_data)
+        true_correction = (uncorrected_data - self.reader0.dark_ref)*self.reader0.gain_ref
+        true_correction[true_correction < 0] = 0
+        true_correction = np.round(true_correction).astype(np.int16)
+        for c,cd in zip(true_correction, corrected_data):
+            print(sum(cd == c))
+        print(corrected_data[-1])
+        print(true_correction[-1])
+        ntest.assert_array_almost_equal(true_correction, corrected_data)
+
+
     def test_parse_metadata(self):
-        self.reader.parse_metadata_file()
+        self.reader0.parse_metadata_file()
 
     def test_load_seq_lazy(self):
         s = load(self.file0, lazy=True)
